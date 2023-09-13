9f0ee238293cf609c407d2b8040844c526865306
B. B. Kreiner <brunokreiner@hotmail.ch>
2023-09-13 16:50:48 +0200

    add initial code structure for checking urls using a cli

--- digilog_experiments/cli.py
+++ digilog_experiments/cli.py
@@ -1,3 +1,7 @@
+import click
+import csv
+from fuzzywuzzy import fuzz
+
 """CLI interface for digilog_experiments project.
 
 Be creative! do whatever you want!
@@ -8,21 +12,34 @@ Be creative! do whatever you want!
 - Import things from your .base module
 """
 
+@click.group()
+def cli():
+    """digilog_experiments CLI."""
+    pass
+
+@click.command()
+@click.argument('csv_path', type=click.Path(exists=True))
+def check(csv_path):
+    """Check each row in a CSV for fuzzy matching between 'website' and 'country'."""
+    with open(csv_path, mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            website = row.get("website", "")
+            country = row.get("country", "")
+            score = fuzz.ratio(website, country)
+            if score > 70:  # you can adjust this threshold as needed
+                click.echo(f"Match found for {website} and {country} with score: {score}")
+            else:
+                click.echo(f"No match found for {website} and {country}.")
+
+cli.add_command(check)
 
 def main():  # pragma: no cover
     """
     The main function executes on commands:
     `python -m digilog_experiments` and `$ digilog_experiments `.
-
-    This is your program's entry point.
-
-    You can change this function to do whatever you want.
-    Examples:
-        * Run a test suite
-        * Run a server
-        * Do some other stuff
-        * Run a command line application (Click, Typer, ArgParse)
-        * List all available tasks
-        * Run an application (Flask, FastAPI, Django, etc.)
     """
-    print("This will do something")
+    cli()
+
+if __name__ == "__main__":
+    main()
