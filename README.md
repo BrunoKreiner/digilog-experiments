2a7b89c6a1cdee487253deae9aed6f04aa3c76fa
B. B. Kreiner <brunokreiner@hotmail.ch>
2023-09-27 13:45:23 +0200

    add url checking exploratory data analysis using decision tree rules and features engineered

--- README.md
+++ README.md
@@ -1,33 +1,3 @@
-# Python Project Template
-
-A low dependency and really simple to start project template for Python Projects.
-
-See also
-
-- [Flask-Project-Template](https://github.com/rochacbruno/flask-project-template/) for a full feature Flask project including database, API, admin interface, etc.
-- [FastAPI-Project-Template](https://github.com/rochacbruno/fastapi-project-template/) The base to start an openapi project featuring: SQLModel, Typer, FastAPI, JWT Token Auth, Interactive Shell, Management Commands.
-
-### What is included on this template?
-
-- 🖼️ Templates for starting multiple application types:
-  * **Basic low dependency** Python program (default) [use this template](https://github.com/rochacbruno/python-project-template/generate)
-  * **Flask** with database, admin interface, restapi and authentication [use this template](https://github.com/rochacbruno/flask-project-template/generate).
-    **or Run `make init` after cloning to generate a new project based on a template.**
-- 📦 A basic [setup.py](setup.py) file to provide installation, packaging and distribution for your project.Template uses setuptools because it's the de-facto standard for Python packages, you can run `make switch-to-poetry` later if you want.
-- 🤖 A [Makefile](Makefile) with the most useful commands to install, test, lint, format and release your project.
-- 📃 Documentation structure using [mkdocs](http://www.mkdocs.org)
-- 💬 Auto generation of change log using **gitchangelog** to keep a HISTORY.md file automatically based on your commit history on every release.
-- 🐋 A simple [Containerfile](Containerfile) to build a container image for your project.`Containerfile` is a more open standard for building container images than Dockerfile, you can use buildah or docker with this file.
-- 🧪 Testing structure using [pytest](https://docs.pytest.org/en/latest/)
-- ✅ Code linting using [flake8](https://flake8.pycqa.org/en/latest/)
-- 📊 Code coverage reports using [codecov](https://about.codecov.io/sign-up/)
-- 🛳️ Automatic release to [PyPI](https://pypi.org) using [twine](https://twine.readthedocs.io/en/latest/) and github actions.
-- 🎯 Entry points to execute your program using `python -m <digilog_experiments>` or `$ digilog_experiments` with basic CLI argument parsing.
-
-<!--  DELETE THE LINES ABOVE THIS AND WRITE YOUR PROJECT README BELOW -->
-
----
-
 # digilog_experiments
 
 [![codecov](https://codecov.io/gh/BrunoKreiner/digilog-experiments/branch/main/graph/badge.svg?token=digilog-experiments_token_here)](https://codecov.io/gh/BrunoKreiner/digilog-experiments)
@@ -43,18 +13,26 @@ pip install digilog_experiments
 
 ## Usage
 
-```py
-from digilog_experiments import BaseClass
-from digilog_experiments import base_function
+cd digilog_experiments
 
-BaseClass().base_method()
-base_function()
-```
+#### python -m cli human-labeling-preprocess:
+
+###### Arguments
+
+1. `csv_path`: Path to the input CSV file. This path must exist.
+2. `output_path`: Path where the preprocessed data will be saved.
+3. `n`: Number of top examples to take after preprocessing.
+
+###### Options
+
+- `--seed`: Random seed for reproducibility. (Default: 42)
+- `--include-luqa`: Include Luqa, Malta in the top n examples. (Default: True)
+- `--shuffle`: Shuffle the data before taking the top n examples. (Default: True)
+
+###### Example
 
 ```bash
-$ python -m digilog_experiments
-#or
-$ digilog_experiments
+$ human_labeling_preprocess ./path/to/input.csv ./path/to/output.csv 100 --seed 123 --include-luqa true --shuffle false
 ```
 
 ## Development
--- digilog_experiments/cli.py
+++ digilog_experiments/cli.py
@@ -1,5 +1,6 @@
 import click
 import csv
+import pandas as pd
 from fuzzywuzzy import fuzz
 
 """CLI interface for digilog_experiments project.
@@ -32,7 +33,87 @@ def check(csv_path):
             else:
                 click.echo(f"No match found for {website} and {country}.")
 
+@click.command()
+@click.argument('csv_path', type=click.Path(exists=True))
+@click.argument('output_path', type=click.Path())
+@click.argument('n', type=int)
+@click.option('--seed', default=42, help='Random seed for reproducibility.')
+@click.option('--include-luqa', default = True, type=click.BOOL, help='Include Luqa, Malta in the top n examples.')
+@click.option('--shuffle', default = True, type=click.BOOL, help='Shuffle the data before taking the top n examples.')
+def human_labeling_preprocess(csv_path, output_path, n, seed, include_luqa, shuffle):
+    """Preprocess a CSV file for human annotation."""
+    df = pd.read_csv(csv_path)
+
+    # Filter out invalid urls:
+    df = df[~df["status"].str.contains("invalid url|no website found|no wikipedia page found", case=False, na=False)]
+
+    if shuffle:
+        df = df.sample(frac=1, random_state=seed)
+
+    # Ensure the "Luqa, Malta" row is included
+    if include_luqa:
+        luqa_row = df[(df["Municipality"] == "Luqa") & (df["Country"] == "Malta")]
+    
+    df = df[df["Website"].notna()]
+
+    # Depending on whether Luqa is already in the shuffled set, we might append or just keep as-is
+    if include_luqa:
+        if not df.head(n).isin(luqa_row.to_dict(orient='list')).all(1).any():
+            df = pd.concat([luqa_row, df]).reset_index(drop=True)
+
+    # Take the top n examples
+    df_top_n = df.head(n)
+    print("shuffle", shuffle)
+    print("include_luqa", include_luqa)
+    # Add the "Label", "Comment", and an example "Confidence" column for human annotators
+    df_top_n['Label'] = ''  # Empty string to start, to be filled in by annotators
+    df_top_n['Comment'] = ''  # Space for annotators to add any notes or comments
+    df_top_n['Confidence'] = ''  # A column where annotators can rate their confidence in their label (e.g., "High", "Medium", "Low")
+
+    # Save the updated dataframe to a new CSV file
+    df_top_n.to_csv(output_path, index=False)
+
+@click.command()
+@click.argument('csv_file', default='../data/HumanAnnotated.csv', type=click.Path(exists=True))
+def label_data(csv_file):
+    df = pd.read_csv(csv_file)
+
+    while True:
+        # Find the first unlabeled row
+        unlabeled_rows = df[df['Label'].isna()]
+        if unlabeled_rows.empty:
+            click.echo("All rows have been labeled!")
+            break
+
+        row = unlabeled_rows.iloc[0]
+
+        # Display the information
+        click.echo(f"Website: {row['Website']}")
+        click.echo(f"Municipality: {row['Municipality']}")
+        click.echo(f"Country: {row['Country']}")
+        click.echo("------------------------------")
+
+        # Get annotations from the user
+        label = click.prompt("Enter label (0 for incorrect, 1 for correct, 2 for website problem)", type=int)
+        comment = click.prompt("Enter any comments", default="")
+        confidence = click.prompt("Enter confidence level (0-100)", type=int)
+
+        # Update the dataframe with the new annotations
+        idx = row.name
+        df.at[idx, 'Label'] = label
+        df.at[idx, 'Comment'] = comment
+        df.at[idx, 'Confidence'] = confidence
+
+        # Save changes to the CSV
+        df.to_csv(csv_file, index=False)
+
+        # Ask if the annotator wants to continue
+        if not click.confirm("Would you like to continue annotating?"):
+            break
+
 cli.add_command(check)
+cli.add_command(human_labeling_preprocess)
+cli.add_command(label_data)
 
 def main():  # pragma: no cover
     """
